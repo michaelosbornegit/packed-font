ce2c0add35a0b3af2e9b6f0757536776393d31e7
Michael Osborne <resonatortune@gmail.com>
2024-11-30 18:57:27 -0800

    include all icons but single erroring unicode 173

--- create/create-dripicons-v2.sh
+++ create/create-dripicons-v2.sh
@@ -1,3 +1,4 @@
 # using package/project manager uv https://docs.astral.sh/uv/
-uv run create-font.py ../fonts/dripicons-16/dripicons-v2.ttf ../fonts/dripicons-16/dripicons-v2-16.json --size 16 --verbose
+# No idea why and don't want to spend more time on it, character code 173 errors. So I just skip it.
+uv run create-font.py ../fonts/dripicons-16/dripicons-v2.ttf ../fonts/dripicons-16/dripicons-v2-16.json --size 16 --chars 0-172,174-200 --verbose
 uv run pack-font.py ../fonts/dripicons-16/dripicons-v2-16.json
